3800bb01209718837125f01549d9558ace9ea7ed
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-03-12 11:27:15 -0400

    exercice

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -1,16 +1,29 @@
-// Créer un tableau de 3 prix
-// Sélectionner toutes les balises de prix
-
+// Sélectionner plusieurs éléments HTML
 // Boucle for, forEach
-// Modifier le contenu de la balise de prix
 
-// Créer une fonction qui prend en paramètre un tableau de prix et qui retourne le prix le plus élevé
-// Appeler la fonction et afficher le prix le plus élevé dans la console
+// Ajouter un élément au tableau
+// Supprimer un élément du tableau
+// Modifier un élément du tableau
+// Afficher un élément du tableau
+// Trouver un élément dans le tableau
+// Trier le tableau
+// Copier un tableau
 
-// Trier le tableau de prix du plus petit au plus grand
-// Afficher le tableau trié dans la console
+//==================
+// Live share - 20 min
+const nomsAlbums = ["Rumours", "Back in Black", "The Dark Side of the Moon", "Nevermind", "The Doors", "Mezzanine"];
+const prix = [15, 30, 15, 40, 18, 15];
 
-// Rechercher un élément dans le tableau de prix
+// 1. Créer une fonction qui retourne le prix d'un album. La fonction prend en paramètre un tableau de noms d'album et un tableau de prix.
+
+// 2. Créer une fonction qui prend en paramètre un tableau de noms d'album et qui retourne un nouveau tableau trié par ordre décroissant
+
+// 3. Créer une fonction qui prend en paramètre un tableau de prix et qui retourne un nouveau tableau sans les doublons
+
+// 4. Créer une fonction qui prend en paramètre un tableau de prix et qui retourne le prix moyen
+
+// 5. Créer une fonction qui retourne un nouveau tableau avec les noms des albums qui ont un prix supérieur à 20
+//===================
 
 // Techniques de débogage
 // opérateur type of
