2e55b469b4b87a8e7e236da0dba6f62bbeea4952
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-01 14:46:35 -0400

    revision

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -1,15 +1,89 @@
-//==== Éléments HTML
-const panierAchat = document.querySelector(".panier-achat");
-//Sélectionner avec attribut, id,classe
-//Sélectionner avec ID
+// Révision
 
-//Fonction pour cloner un élément du panier achat
-//Au double-clic, ajouter l'élément au panier achat
-//Sélectionner le parent et supprimer au survol
+// ==== Révision des boucles
+// À l'aide d'une boucle for:
+// Afficher les nombres de 0 à 10 dans la console
+// Créer un tableau de chaines de caractères et afficher chaque élément dans la console
+// Récupérer tous les éléments avec la classe bouton et afficher chaque élément dans la console
 
-//=== Fonctions
-function initialiser() {}
+// ==== Révision des fonctions
+let tableauNombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Créer une fonction qui prend en paramètre le tableau et qui retourne la somme de tous les éléments du tableau
+// Créer une fonction qui prend en paramètre le tableau et qui retourne la moyenne de tous les éléments du tableau
+// Créer une fonction qui prend en paramètre le tableau et qui retourne le plus grand nombre du tableau
+// Créer une fonction qui prend en paramètre le tableau et un nombre et qui retourne tous les nombres plus petits ou égaux que le nombre passé en paramètre
 
-function clonerElement() {}
+// ==== Révision des chaines de caractères
+// Créer une fonction qui prend en paramètre une chaine de caractères et qui retourne la chaine de caractères inversée
+// Créer une fonction qui prend en paramètre une chaine de caractères et qui retourne la chaine de caractères nettoyée de tous les espaces et dont les mots sont séparés par un trait d'union
+// Créer une fonction qui prend en paramètre une chaine de caractères et qui retourne la chaine de caractères avec la première lettre de chaque mot en majuscule
 
-initialiser();
+// ==== Révision des tableaux
+let tableauAnimauxTerrestres = [
+    "chien",
+    "chat",
+    "souris",
+    "lapin",
+    "cheval",
+    "vache",
+    "mouton",
+    "cochon",
+    "poule",
+    "canard",
+];
+let tableauAnimauxMarins = [
+    "requin",
+    "dauphin",
+    "baleine",
+    "méduse",
+    "poisson",
+    "pieuvre",
+    "moule",
+    "crabe",
+    "étoile de mer",
+    "hippocampe",
+];
+// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments uniques
+// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments en double
+// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments triés par ordre alphabétique
+// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments triés par ordre décroissant
+// Créer une fonction qui prend en paramètre les deux tableaux et qui retourne un tableau fusionné
+// Créer une fonction qui prend en paramètre un tableau et qui filtre (retire) les mots de moins de 5 lettres
+
+// ==== Révision des objets
+let etudiants = [
+    {
+        nom: "Pigeon",
+        prenom: "Maxime",
+        age: 25,
+        resultats: [12, 15, 18, 9, 13],
+    },
+    {
+        nom: "Chartier",
+        prenom: "Marc-André",
+        age: 22,
+        resultats: [8, 10, 12, 11, 9],
+    },
+    {
+        nom: "Harvey",
+        prenom: "Guillaume",
+        age: 40,
+        resultats: [15, 16, 17, 14, 18],
+    },
+    {
+        nom: "Pouliot",
+        prenom: "Stéphanie",
+        age: 35,
+        resultats: [10, 11, 12, 9, 13],
+    },
+];
+// Créer une fonction qui prend en paramètre un tableau de résultats et qui retourne la moyenne des résultats d'un étudiant
+// Créer une fonction qui prend en paramètre un tableau d'étudiants et qui retourne la moyenne des résultats du groupe (réutiliser la fonction précédente)
+// Créer une fonction qui prend en paramètre un tableau d'étudiants et qui retourne l'étudiant avec la meilleure moyenne
+// Créer une fonction qui prend en paramètre un tableau d'étudiants et qui filtre les étudiants plus jeunes que 35 ans
+
+// ==== Révision des événements
+// Créer une fonction qui affiche la liste des étudiants sur la page HTML
+// Appeler la fonction pour afficher la liste des étudiants au chargement de la page
+// Créer une fonction qui ajoute un étudiant à la liste et réaffiche la liste
+// Créer une fonction qui supprime le dernier étudiant de la liste et réaffiche la liste
