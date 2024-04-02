37a83bf5a2312b9476ea631e0dfbaa4a6fafd87a
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-01 20:10:23 -0400

    revision

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -3,12 +3,10 @@
 // ==== Révision des boucles
 // À l'aide d'une boucle for:
 // Afficher les nombres de 0 à 10 dans la console
-// Créer un tableau de chaines de caractères et afficher chaque élément dans la console
 // Récupérer tous les éléments avec la classe bouton et afficher chaque élément dans la console
 
 // ==== Révision des fonctions
 let tableauNombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// Créer une fonction qui prend en paramètre le tableau et qui retourne la somme de tous les éléments du tableau
 // Créer une fonction qui prend en paramètre le tableau et qui retourne la moyenne de tous les éléments du tableau
 // Créer une fonction qui prend en paramètre le tableau et qui retourne le plus grand nombre du tableau
 // Créer une fonction qui prend en paramètre le tableau et un nombre et qui retourne tous les nombres plus petits ou égaux que le nombre passé en paramètre
@@ -43,12 +41,10 @@ let tableauAnimauxMarins = [
     "étoile de mer",
     "hippocampe",
 ];
-// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments uniques
-// Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments en double
 // Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments triés par ordre alphabétique
 // Créer une fonction qui prend en paramètre le tableau et qui retourne un tableau avec les éléments triés par ordre décroissant
 // Créer une fonction qui prend en paramètre les deux tableaux et qui retourne un tableau fusionné
-// Créer une fonction qui prend en paramètre un tableau et qui filtre (retire) les mots de moins de 5 lettres
+// Créer une fonction qui prend en paramètre un tableau et qui filtre (retire) les mots de moins de 5 lettres et retourne le reste
 
 // ==== Révision des objets
 let etudiants = [
@@ -56,25 +52,31 @@ let etudiants = [
         nom: "Pigeon",
         prenom: "Maxime",
         age: 25,
-        resultats: [12, 15, 18, 9, 13],
+        resultats: [75, 80, 85, 70, 90],
+    },
+    {
+        nom: "Lacasse",
+        prenom: "Maxime",
+        age: 40,
+        resultats: [67, 72, 78, 65, 80],
     },
     {
         nom: "Chartier",
         prenom: "Marc-André",
         age: 22,
-        resultats: [8, 10, 12, 11, 9],
+        resultats: [67, 72, 96, 76, 80],
     },
     {
         nom: "Harvey",
         prenom: "Guillaume",
-        age: 40,
-        resultats: [15, 16, 17, 14, 18],
+        age: 43,
+        resultats: [100, 45, 78, 65, 80],
     },
     {
         nom: "Pouliot",
         prenom: "Stéphanie",
         age: 35,
-        resultats: [10, 11, 12, 9, 13],
+        resultats: [96, 84, 76, 65, 100],
     },
 ];
 // Créer une fonction qui prend en paramètre un tableau de résultats et qui retourne la moyenne des résultats d'un étudiant
@@ -87,3 +89,5 @@ let etudiants = [
 // Appeler la fonction pour afficher la liste des étudiants au chargement de la page
 // Créer une fonction qui ajoute un étudiant à la liste et réaffiche la liste
 // Créer une fonction qui supprime le dernier étudiant de la liste et réaffiche la liste
+// Créer une fonction qui trie les étudiants par ordre alphabétique et réaffiche la liste
+// Au clic des boutons, appeler les fonctions pour trier les étudiants
