c4ce745ba0442b048d0f295385c6fe83732d077b
Maxime Lacasse Germain <mlacassegermain@cmaisonneuve.qc.ca>
2024-05-08 13:10:27 -0400

    Cours 16 correction 2

--- 02_Demo/assets/js/pages/quiz.js
+++ 02_Demo/assets/js/pages/quiz.js
@@ -1,10 +1,10 @@
 import { init as navigationInit } from "../modules/navigation.js";
-import Quiz from "../classes/Quiz.js"
+import Quiz from "../classes/Quiz.js";
 
 function init() {
   navigationInit();
 
-  let quiz = new Quiz();
+  // let quiz = new Quiz();
 }
 
 init();
