c647417ef659daa98500115474d69aabdc33694b
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-02-03 16:07:48 -0500

    changement

--- 02_Demo/assets/js/main.js
+++ 02_Demo/assets/js/main.js
@@ -1,16 +1,20 @@
-// Variables
-
+// === Variables ===
 const chemin = ["assets", "img", "albums"];
 const albums = ["Mezzanine", "Back in Black", "The Dark Side of the Moon", "Abbey Road", "Kind of Blue", "The Wall"];
 
 // const template = `
 // <div class="album" id="">
-//     <img src="assets/img/albums/doors_the_doors.webp" alt="" />
+//     <img src="Ajouter le chemin correctement formatté" alt="" />
 // </div>`;
 
-// Créer une fonction qui formatte le chemin des images
-// Créer une fonction qui génère le template HTML et l'injecte dans le DOM
+// === Fonctions ===
+function init() {}
+// Créer une fonction qui formatte le chemin des images. Ex: assets/img/albums/doors_the_doors.webp
+// Créer une fonction qui génère le gabarit HTML et l'injecte dans le DOM
 // Créer une fonction qui affiche les albums au chargement de la page
 
 // Au clic du bouton, vider le contenu du conteneur et réafficher les albums
 // Au clic d'une div album, afficher le id dans la console et afficher l'attribut alt de l'image dans la console
+
+// === Exécution ===
+init();
